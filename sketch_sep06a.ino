4e02ce64b167b865601065e39c112838598609d8
muhammedalys <muhammed_aly_ozen@hotmail.com>
2018-10-01 09:29:54 +0300

    Add files via upload

---
+++ sketch_sep06a.ino
@@ -0,0 +1,19 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 20;
+
+// Connection pins:
+Stepper myStepperX(stepsPerRevolution, 8,9,10,11);
+
+void setup() {
+// Set speed:
+myStepperX.setSpeed(200);
+// max 250 steps for dvd/cd stepper motor
+myStepperX.step(125);
+delay(100);
+
+}
+
+void loop() {
+
+}
